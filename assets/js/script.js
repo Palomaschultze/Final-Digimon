2e2fad07336776e2ed34fd05f59db855ebf1ab00
paloma Schultze <palomaschultze@gmail.com>
2023-04-16 20:17:36 -0400

    Se agrega el proyecto

---
+++ assets/js/script.js
@@ -0,0 +1,46 @@
+
+function cargarDigimones() {
+    let url = "https://digimon-api.vercel.app/api/digimon";
+    fetch(url)
+        .then((response) => response.json())
+        .then((data) => {
+            let registro = "";
+            data.forEach((digimon,index) => {
+                registro +=`
+                <tr>
+                    <td>${index + 1}</td>
+                    <td>${digimon.name}</td>
+                    <td>${digimon.level}</td>
+                    <td><button class="btn btn-primary" 
+                    onclick="mostrarModal('${digimon.name}', '${digimon.level}','${digimon.img}',)">Ver Digimon</button></td>
+                </tr>`;
+            });
+
+            let datos = document.getElementById("tablaDigimones");
+            datos.innerHTML = registro; 
+            
+        })
+        .catch((error) => {
+            console.error(error);
+        });
+}
+cargarDigimones()
+
+function mostrarModal(name, level, img) {
+    let staticBackdropLabel = document.getElementById("staticBackdropLabel");
+    staticBackdropLabel.innerText = name;
+    let modalLevel = document.getElementById("modalLevel");
+    modalLevel.innerText = level;
+    let modalImg = document.getElementById("imagenDigi");
+    modalImg.setAttribute("src", img )
+    const myModal = new bootstrap.Modal("#staticBackdrop");
+    myModal.show();
+  }
+
+
+  function buscarDigimon(){
+    let buscardor = document.getElementById("buscador");
+    buscardor.innerHTML
+  }
+
+
